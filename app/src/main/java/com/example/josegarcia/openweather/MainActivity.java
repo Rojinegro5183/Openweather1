3b50d9b5e74462296b5d4d2722a87ecbb866c248
Jose Roberto Garcia Leon <jose.garcia@infotec.mx>
2018-05-04 12:36:40 -0500

    Imagen y texto

--- app/src/main/java/com/example/josegarcia/openweather/MainActivity.java
+++ app/src/main/java/com/example/josegarcia/openweather/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.josegarcia.openweather;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView iv_weather = (ImageView)findViewById(R.id.iv_weather);
+        int identificador = getResources().getIdentifier("imagen02d","drawable",getPackageName());
+        iv_weather.setImageDrawable(getResources().getDrawable(identificador,null));
+
+
     }
+
 }
